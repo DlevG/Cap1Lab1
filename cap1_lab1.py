ba4450c7b28a644b6838117dfef9bdf8627d04a8
DlevG <95711335+DlevG@users.noreply.github.com>
2021-12-07 21:13:21 +0200

    Add files via upload

---
+++ cap1_lab1.py
@@ -0,0 +1,4 @@
+print(WhatError)#NameError: name 'WhatError' is not defined. Did you mean: 'TabError'?
+Print(‘what error is this’)#SyntaxError: invalid character '‘' (U+2018)
+print(what error)#SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print(‘what error is this)#SyntaxError: invalid syntax. Perhaps you forgot a comma?
